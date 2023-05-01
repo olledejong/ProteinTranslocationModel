7413d99e43eb58bd8552c08bd4ead1a6748dee4e
Olle de Jong <olledejong@gmail.com>
2023-05-01 09:46:37 +0200

    Latest changes

--- model.py
+++ model.py
@@ -2,16 +2,22 @@ import sys
 import plotting
 import numpy as np
 import pandas as pd
+from math import log
 from scipy.integrate import odeint
 from scipy.interpolate import CubicSpline, interp1d, UnivariateSpline
 
-peak_of_nuc_vol = 80  # the latest local high of the nuclear volume
+peak_of_nuc_vol = 81  # the latest local high of the nuclear volume
 nuc_div_tp = 91  # simulated point at which nuclear division takes place
 
 
 #########################
 ### Data preparations ###
 #########################
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
 
 
 def load_data():
@@ -56,12 +62,14 @@ def alter_data(averages):
 ###############################
 
 
-def dptd(y, t, kd, kc, kImp, kExp):
+def dptd(y, t, kd, kc, kIn, kOut):
     c, n = y  # cellular and nuclear abundance of protein
+    kIn_f = kIn / a_func(t)
+    kOut_f = kOut / a_func(t)
 
     # system equations, describe the change in cellular and nuclear abundances
-    dC_dt = kc * 50 + a_func(t) * (-kImp * c / (cv_func(t) - nv_func(t)) + kExp * a_func(t) / nv_func(t)) - kd * c
-    dN_dt = a_func(t) * (kImp * c / (cv_func(t) - nv_func(t)) - kExp * n / nv_func(t)) - kd * n
+    dC_dt = kc * 50 + a_func(t) * (-kIn_f * c / (cv_func(t) - nv_func(t)) + kOut_f * a_func(t) / nv_func(t)) - kd * c
+    dN_dt = a_func(t) * (kIn_f * c / (cv_func(t) - nv_func(t)) - kOut_f * n / nv_func(t)) - kd * n
 
     return [dC_dt, dN_dt]
 
@@ -75,17 +83,21 @@ nv_func = interp1d(t_range, nuc_vols, kind='linear', bounds_error=False)
 
 
 def main():
-    kd = 0.2  # degradation rate for protein
-    kc = 0.6  # synthesis rate of protein in cytosol
-    kin = 0.8  # rate of translocation into nucleus
-    kout = 0.2  # rate of translocation out of nucleus
+    kd = log(2)/35  # degradation rate for protein
+    kc = 0.6  # synthesis rate of protein in cytoplasm
+    kin = log(2)/10  # rate of translocation into nucleus
+    kout = log(2)/10  # rate of translocation out of nucleus
 
     # initial conditions
-    cp0 = 91.08659014056978
-    np0 = 28.248611459856743
+    cp0 = 1425.23317845
+    np0 = 50.99743377
+
+    # generate split t_span to be able to simulate nuclear division event at 'nuc_div_tp'
+    tspan_whole = np.linspace(0, 99, 200)
+    closest = find_nearest(tspan_whole, nuc_div_tp)
+    tspan_before, tspan_after = np.split(tspan_whole, np.where(tspan_whole == closest)[0] + 1)
 
     # solve the ode up until the event
-    tspan_before = np.linspace(0.0, 91, 200)
     ini_cond = np.array([cp0, np0])
     sols_before = odeint(dptd, ini_cond, tspan_before, args=(kd, kc, kin, kout))
 
@@ -97,7 +109,6 @@ def main():
     ])
 
     # simulate part after the event
-    tspan_after = np.linspace(91, 98, 20)
     sols_after = odeint(dptd, sols_after_event, tspan_after, args=(kd, kc, kin, kout))
 
     # final values (concatenated)
@@ -108,6 +119,7 @@ def main():
     plotting.plot_abundances(final_tspan, final_sols[:, 0], final_sols[:, 1])
     plotting.plot_volume_ratio(t_range, nuc_vols, cell_vols)
     plotting.plot_abundance_ratio(final_tspan, final_sols)
+    plotting.plot_concentration_ratio(final_tspan, final_sols, cv_func, nv_func)
 
 
 if __name__ == '__main__':
--- plotting.py
+++ plotting.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_abundances(tspan, y1, y2):
     fig, ax1 = plt.subplots()
-    fig.suptitle("Cytosolic and nuclear protein abundances over time")
+    fig.suptitle("Cytoplasmic and nuclear protein abundances over time")
     ax1.set_xlabel('Time')
     ax1.grid(False)
-    ax1.set_ylabel("Cytosolic protein abundance", color='orange')
+    ax1.set_ylabel("Cytoplasmic protein abundance", color='orange')
     ax1.plot(tspan, y1, color='orange')
     ax1.tick_params(axis='y', labelcolor='orange')
     ax2 = ax1.twinx()
@@ -28,6 +29,52 @@ def plot_volume_ratio(t_range, nuc_vols, cell_vols):
 def plot_abundance_ratio(final_tspan, final_sols):
     plt.plot(final_tspan, final_sols[:, 1] / final_sols[:, 0], color='red', lw=2)
     plt.title("Nuclear to cytoplasmic abundance ratio")
+    plt.xlabel("Time")
+    plt.ylabel("Ratio")
+    plt.show()
+
+
+def plot_concentration_ratio(final_tspan, final_sols, cv_func, nv_func):
+    # get cell / nuc volumes for all timepoints
+    num_nans = np.count_nonzero(np.isnan(final_sols[:, 0]))
+    cell_vols = [cv_func(t).flatten()[0] for t in final_tspan][:-num_nans]
+    nuc_vols = [nv_func(t).flatten()[0] for t in final_tspan][:-num_nans]
+    cyt_vols = [i - j for i, j in zip(cell_vols, nuc_vols)]
+
+
+    # with the abundances, the event is distinctly happening over one time-step, this is not the case for the volumes.
+    # this is because the volumes are first manipulated to represent nuclear division at one time-step, but after that
+    # they are interpolated using the interp1d method. This causes a more transient simulation of that division.
+    # therefore we need to slightly alter these to prevent plotting artifacts.
+    nuc_vols[181:184] = [nuc_vols[180]] * len(nuc_vols[181:184])  # TODO make this dynamic (non-hardcoded indexes)
+
+    # get abundances
+    c_ab = final_sols[:, 0].tolist()[:-num_nans]
+    n_ab = final_sols[:, 1].tolist()[:-num_nans]
+
+    # calculate the concentrations
+    c_con = [i / j for i, j in zip(c_ab, cyt_vols)]
+    n_con = [i / j for i, j in zip(n_ab, nuc_vols)]
+
+    con_ratio = [i / j for i, j in zip(n_con, c_con)]
+
+    plt.plot(final_tspan[:-num_nans], c_con, c='red', lw=2)
+    plt.title("Cytoplasmic protein concentration")
+    plt.xlabel("Time")
+    plt.ylabel("Concentration")
+    plt.show()
+
+    print(n_ab[180:185])
+    print(nuc_vols[180:185])
+    print(n_con[180:185])
+    plt.plot(final_tspan[:-num_nans], n_con, c='red', lw=2)
+    plt.title("Nuclear protein concentration")
+    plt.xlabel("Time")
+    plt.ylabel("Concentration")
+    plt.show()
+
+    plt.plot(final_tspan[:-num_nans], con_ratio, c='red', lw=2)
+    plt.title("Nuclear to cytoplasmic protein concentration ratio")
     plt.xlabel("Time")
     plt.ylabel("Ratio")
     plt.show()
